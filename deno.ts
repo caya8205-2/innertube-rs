680be9cac246d88a77690408200b5018a8e86fc7
caya8205-2 <yaandere200@gmail.com>
2026-08-24 07:14:09 +0700

    chore(cleanup): remove legacy typescript files, preserve docs and keep reference in reference-youtubejs branch

--- deno.ts
+++
@@ -1,3 +0,0 @@
-export * from './deno/src/platform/deno.ts';
-import Innertube from './deno/src/platform/deno.ts';
-export default Innertube;
--- src/parser/classes/misc/AccessibilityContext.ts
+++
@@ -1,9 +0,0 @@
-import type { RawNode } from '../../types/index.js';
-
-export default class AccessibilityContext {
-  public label: string;
-
-  constructor(data: RawNode) {
-    this.label = data.label;
-  }
-} 
